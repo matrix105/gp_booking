57e42ba6c55fbed357e70a417a18db1d3750b92e
Deepesh Patel <70107741+deepesh2408@users.noreply.github.com>
2021-02-05 13:11:12 +0000

    Database Tables

--- gp_booking/core/models.py
+++ gp_booking/core/models.py
@@ -1,7 +1,7 @@
 from django.db import models 
+from django.core.validators import MaxValueValidator, MinValueValidator 
 from datetime import date
 from datetime import time
-from pony.orm import *
 
   
 # Create your models here. 
@@ -11,47 +11,64 @@ class React(models.Model):
     name = models.CharField(max_length=30) 
     detail = models.CharField(max_length=500)
 
-db = Database()
-db.bind(provider='sqlite', filename='../db.sqlite3', create_db=True)
+class Patient(models.Model):
+    nhs_num = models.IntegerField(primary_key=int)
+    #nhs_num = Required(int)
+    email = models.EmailField(max_length=254, unique=True)
+    #email = Required(str, unique=True)
+    passworsd=models.CharField(max_length=30)
+    #password = Required(str)
+    firstname= models.CharField(max_length=50)
+    #name = Optional(str)
+    lastname=models.CharField(max_length=50)
+    #lastname = Optional(str)
+    dob=models.DateField()
+    #dob = Optional(str)
+    phone= models.IntegerField(validators=[MinValueValidator(999999999), MaxValueValidator(9999999999)])
+    #phone = Optional(str)
+    bookings= models.SET('Booking')
+    #bookings = Set('Booking')
+    notes= models.CharField(max_length=9999)
+    #notes = Optional(str)
+
+class Doctor(models.Model):
+    nhs_num = models.IntegerField(primary_key=int)
+    #nhs_num = PrimaryKey(int, auto=True)
+    email = models.EmailField(max_length=254, unique=True)
+    #email = Required(str)
+    password=models.CharField(max_length=30)
+    #password = Required(str)
+    firstname=models.CharField(max_length=50)
+    #name = Optional(str)
+    lastname=models.CharField(max_length=50)
+    #lastname = Optional(str)
+    dob=models.DateField()
+    #dob = Optional(str)
+    phone= models.IntegerField(validators=[MinValueValidator(999999999), MaxValueValidator(9999999999)])
+    #phone = Optional(str)
+
+class Booking(models.Model):
+    id=models.IntegerField(primary_key=int,auto_created=True)
+    #id = PrimaryKey(int, auto=True)
+    patient= models.ForeignKey(Patient,on_delete=models.CASCADE)
+    #patient = Required(Patient)
+    date=models.DateField()
+    #date = Required(date)
+    time=models.TimeField()
+    #time = Required(time)
+    doctor=models.ForeignKey(Doctor,on_delete=models.CASCADE)
+    #doctor = Optional(str)
+    notes=models.CharField(max_length=9999)
+    #notes = Optional(LongStr)
 
 
-class Patient(db.Entity):
-    nhs_num = Required(int)
-    email = Required(str, unique=True)
-    password = Required(str)
-    name = Optional(str)
-    lastname = Optional(str)
-    dob = Optional(str)
-    phone = Optional(str)
-    bookings = Set('Booking')
-    notes = Optional(str)
+class WorkingHour(models.Model):
+    id=models.IntegerField(primary_key=int,auto_created=True)
+    #id = PrimaryKey(int, auto=True)
+    startTime=models.TimeField()
+    #startTime = Required(time)
+    endTime=models.TimeField()
+    #endTime = Required(time)
+    quantity=models.IntegerField(default=4)
+    #quantity = Required(int, default=4)
 
-
-class Booking(db.Entity):
-    id = PrimaryKey(int, auto=True)
-    patient = Required(Patient)
-    date = Required(date)
-    time = Required(time)
-    doctor = Optional(str)
-    notes = Optional(LongStr)
-
-
-class Doctor(db.Entity):
-    nhs_num = PrimaryKey(int, auto=True)
-    email = Required(str)
-    password = Required(str)
-    name = Optional(str)
-    lastname = Optional(str)
-    dob = Optional(str)
-    phone = Optional(str)
-
-
-class WorkingHour(db.Entity):
-    id = PrimaryKey(int, auto=True)
-    startTime = Required(time)
-    endTime = Required(time)
-    quantity = Required(int, default=4)
-
-
-
-db.generate_mapping(create_tables=True)
--- gp_booking/core/serializer.py
+++ gp_booking/core/serializer.py
@@ -5,3 +5,18 @@ class ReactSerializer(serializers.ModelSerializer):
     class Meta: 
         model = React 
         fields = ['name', 'detail'] 
+    class Patient: 
+        model = Patient 
+        fields = ['nhs_num', 'email','password','firstname','lastname','dob','phone','bookings','notes']
+    class Doctor: 
+        model = Doctor 
+        fields = ['nhs_num','email','password','firstname','lastname','dob','phone']
+    class Booking: 
+        model = Booking 
+        fields = ['id','patient','date','time','doctor','notes']
+    class WorkingHour: 
+        model = WorkingHour 
+        fields = ['id','startTime','endTime','quantity'] 
+ 
+
+
