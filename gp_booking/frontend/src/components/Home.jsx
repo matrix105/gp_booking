4cf2fb8243328d76a611d5bf473e12b56dbd7dda
MT <matrix105.mt@gmail.com>
2021-02-05 19:34:13 +0000

    frontend setup

---
+++ gp_booking/frontend/src/components/Home.jsx
@@ -0,0 +1,14 @@
+import * as React from "react";
+
+export default class Home extends React.Component {
+    render() { 
+        return (
+            <div>
+                <h1>HI</h1>
+                <p><a href="https://google.com">This is a Link in an anchor</a></p>
+            </div>
+		  );
+        
+        };
+}
+
---
+++ gp_booking/frontend/src/components/Layout.tsx
@@ -0,0 +1,12 @@
+import * as React from "react";
+
+
+export default (props: { children?: React.ReactNode }) => (
+
+  <React.Fragment>
+
+    {props.children}
+
+  </React.Fragment>
+
+);
