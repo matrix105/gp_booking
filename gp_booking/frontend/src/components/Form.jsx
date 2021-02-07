38ee831bc2ce94386ca11542f72edd5266f3a42e
Jubeen Amatya <45592860+jubain@users.noreply.github.com>
2021-02-07 19:26:46 +0000

    made way to use same layout for login and signup

--- gp_booking/frontend/src/components/Form.jsx
+++ gp_booking/frontend/src/components/Form.jsx
@@ -3,12 +3,26 @@ import './css/login.css'
 import Image from './mini Compnents/Image'
 import Input from './mini Compnents/Input'
 import Buttons from './mini Compnents/Buttons'
-import loginInput from './mini Compnents/inputs'
+import { loginInput, registerInput } from './mini Compnents/inputs'
 import Messages from './mini Compnents/Messages'
 import { Box } from '@material-ui/core';
 
-const Form = () => {
-
+const Form = (props) => {
+    const loginOrSignup = {}
+    if (props.params === "/login") {
+        loginOrSignup = loginInput
+    } else {
+        loginOrSignup = registerInput
+    }
+    function createImage(image) {
+        return (
+            <Image
+                className="img"
+                alt={image.alt}
+                src={image.img}
+            />
+        )
+    }
     function createInput(input) {
         return (
             <Input
@@ -37,30 +51,25 @@ const Form = () => {
     return (
         <div className="containers" style={{ margin: "0px", padding: "0px" }}>
             <div className="imageContainer">
-                <Image
-                    className="img"
-                    alt={loginInput.alt}
-                    src={loginInput.img}
-                />
+                {(loginOrSignup.messages).map(createImage)}
                 <div className="middle">
                     <div className="text">
-                        {(loginInput.messages).map(createMessage)}
+                        {(loginOrSignup.messages).map(createMessage)}
                     </div>
 
                 </div>
             </div>
             <form autoComplete="off" className="form">
                 <Box mt={5}>
-                    <h1>{loginInput.title}</h1>
+                    <h1>{loginOrSignup.title}</h1>
                     <Box mb={4} mt={5}>
-                        {(loginInput.inputs).map(createInput)}
+                        {(loginOrSignup.inputs).map(createInput)}
                     </Box>
 
                     <div className="buttons">
                         <Box mb={4}>
-                            {(loginInput.buttons.map(createButton))}
+                            {(loginOrSignup.buttons.map(createButton))}
                         </Box>
-
                     </div>
                 </Box>
             </form>
