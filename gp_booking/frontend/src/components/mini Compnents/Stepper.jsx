79da68027f582a5df22d06d5c33d38ee3da302f5
Jubeen Amatya <45592860+jubain@users.noreply.github.com>
2021-02-12 14:25:50 +0000

    Booking page

--- gp_booking/frontend/src/components/mini Compnents/Stepper.jsx
+++ gp_booking/frontend/src/components/mini Compnents/Stepper.jsx
@@ -4,14 +4,18 @@ import Stepper from '@material-ui/core/Stepper';
 import Step from '@material-ui/core/Step';
 import StepLabel from '@material-ui/core/StepLabel';
 import Button from '@material-ui/core/Button';
-import { func } from "prop-types";
+
 import Typography from '@material-ui/core/Typography';
 import TextField from '@material-ui/core/TextField';
+import { FormControl, Select, MenuItem, InputLabel } from '@material-ui/core';
 
 
 const useStyles = makeStyles((theme) => ({
     root: {
         width: '100%',
+    },
+    list: {
+        width: '25%'
     },
     backButton: {
         marginRight: theme.spacing(1),
@@ -31,6 +35,43 @@ const useStyles = makeStyles((theme) => ({
     },
 }));
 
+const availableTimes = [
+    {
+        time: "2:00 PM"
+    },
+    {
+        time: "3:00 PM"
+    },
+    {
+        time: "4:00 PM"
+    },
+    {
+        time: "5:00 PM"
+    },
+    {
+        time: "6:00 PM"
+    },
+    {
+        time: "7:00 PM"
+    },
+    {
+        time: "8:00 PM"
+    },
+    {
+        time: "9:00 PM"
+    },
+    {
+        time: "1:00 PM"
+    },
+    {
+        time: "12:00 AM"
+    },
+    {
+        time: "1:00 AM"
+    },
+]
+
+
 function getCurrentDate() {
     var currentTime = new Date().toISOString().slice(0, 10);
     return currentTime
@@ -41,7 +82,28 @@ function getSteps() {
     return ['Pick a booking date', 'Pick a time slot', 'Book'];
 }
 
-function getStepContent(stepIndex, classes) {
+
+
+function getStepContent(stepIndex, classes, setTime, setOpen, time, open) {
+
+    const handleChange = (event) => {
+        setTime(event.target.value);
+    };
+
+    const handleClose = () => {
+        setOpen(false);
+    };
+
+    const handleOpen = () => {
+        setOpen(true);
+    };
+
+    function getTimeListItem(time) {
+        return (
+            <MenuItem value={time.time}>{time.time}</MenuItem>
+        )
+    }
+
 
     switch (stepIndex) {
         case 0:
@@ -50,8 +112,8 @@ function getStepContent(stepIndex, classes) {
                     <TextField
                         id="date"
                         InputProps={{ inputProps: { min: getCurrentDate() } }}
-                        label="Pick a suitable date"
                         type="date"
+                        label="Select a suitable date"
                         defaultValue={getCurrentDate()}
                         className={classes.textField}
                         InputLabelProps={{
@@ -61,9 +123,26 @@ function getStepContent(stepIndex, classes) {
                 </form>
             );
         case 1:
-            return 'What is an ad group anyways?';
+            return (
+                <FormControl className={classes.list}>
+                    <InputLabel id="demo-controlled-open-select-label">Time</InputLabel>
+                    <Select
+                        labelId="demo-controlled-open-select-label"
+                        id="demo-controlled-open-select"
+                        open={open}
+                        onClose={handleClose}
+                        onOpen={handleOpen}
+                        value={time}
+                        onChange={handleChange}
+                        required
+                    >
+                        {availableTimes.map(getTimeListItem)}
+
+                    </Select>
+                </FormControl>
+            )
         case 2:
-            return 'This is the bit I really care about!';
+            return 'Confirm your appointment booking?';
         default:
             return 'Unknown stepIndex';
     }
@@ -75,6 +154,9 @@ const Steppers = () => {
     const classes = useStyles();
     const [activeStep, setActiveStep] = React.useState(0);
     const steps = getSteps();
+
+    const [time, setTime] = React.useState('');
+    const [open, setOpen] = React.useState(false);
 
     const handleNext = () => {
         setActiveStep((prevActiveStep) => prevActiveStep + 1);
@@ -100,12 +182,13 @@ const Steppers = () => {
             <div>
                 {activeStep === steps.length ? (
                     <div>
+                        <b>Redirect to home page</b>
                         <Typography className={classes.instructions}>All steps completed</Typography>
                         <Button onClick={handleReset}>Reset</Button>
                     </div>
                 ) : (
                         <div>
-                            <Typography className={classes.instructions}>{getStepContent(activeStep, classes)}</Typography>
+                            <Typography className={classes.instructions}>{getStepContent(activeStep, classes, setTime, setOpen, time, open)}</Typography>
                             <div>
                                 <Button
                                     disabled={activeStep === 0}
