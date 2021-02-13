82ec0ff6f7a59971f60cc078981db190d8aedc18
Jubeen Amatya <45592860+jubain@users.noreply.github.com>
2021-02-13 13:10:48 +0000

    edit login

--- gp_booking/frontend/src/components/mini Compnents/Buttons.jsx
+++ gp_booking/frontend/src/components/mini Compnents/Buttons.jsx
@@ -6,7 +6,7 @@ const Buttons = (props) => {
 
     return (
         <Box mb={4}>
-            <Button variant="contained" fullWidth={true} type={props.type} color={props.color} onClick={props.onClick}>{props.name}</Button>
+            <Button variant="contained" fullWidth={true} type={props.type} color={props.color} id={props.id} >{props.name}</Button>
         </Box>
     )
 }
