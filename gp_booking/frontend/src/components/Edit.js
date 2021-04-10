2e8227e7da012a18dca6682e435c9de97bf98a1d
Jubeen Amatya <45592860+jubain@users.noreply.github.com>
2021-04-10 18:14:15 +0100

    ss

--- gp_booking/frontend/src/components/Edit.js
+++ gp_booking/frontend/src/components/Edit.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react';
-import { makeStyles, TextField } from '@material-ui/core';
+import { makeStyles, TextField, InputLabel } from '@material-ui/core';
 import axios from 'axios'
 
 const useStyles = makeStyles((theme) => ({
@@ -13,14 +13,32 @@ const useStyles = makeStyles((theme) => ({
     container: {
         display: 'flex',
         justifyContent: 'center',
+    },
+    textField: {
+        fontSize: 100
     }
 }));
+
+const fontsize = 15
+const labelsize = 20
+
 
 
 function Edit(props) {
     const classes = useStyles();
 
     var userDetailArray
+
+    const [state, setstate] = useState(
+        {
+            fname: '',
+            lname: '',
+            dob: '',
+            phone: '',
+            address: '',
+        })
+
+    const [editable, seteditable] = useState(false)
 
     const initialState = () => {
         const userId = localStorage.getItem('username')
@@ -31,20 +49,20 @@ function Edit(props) {
             },
         }).then((res) => {
             res.data.map(infos => {
-                userDetailArray = infos
-                console.log(userDetailArray.fname);
+                setstate({
+                    fname: infos.fname,
+                    lname: infos.lname,
+                    dob: infos.dob,
+                    phone: infos.phone,
+                    address: infos.address,
+                })
             })
         })
     }
 
-    const [state, setstate] = useState({
-        fname: userDetailArray.fname,
-        lname: userDetailArray.lname,
-        dob: userDetailArray.dob,
-        phone: userDetailArray.phone,
-        address: userDetailArray.address,
-    })
-
+    const onTextChange = (name) => e => {
+        setstate({ ...state, [name]: e.target.value })
+    }
 
     const createInput = () => {
         const tempArray = []
@@ -55,20 +73,47 @@ function Edit(props) {
             tempArray.map(input => {
                 switch (input) {
                     case 'dob':
-                        return <TextField id="outlined-basic" variant="outlined" value={state.dob} key={input} type='date' />
+                        return (
+
+                            <TextField id="outlined-basic" label={input} variant="outlined" value={state.dob} key={input} type='date' className={classes.textField} InputLabelProps={{ style: { fontSize: labelsize } }} inputProps={{ style: { fontSize: fontsize } }} />
+
+                        )
                     case 'phone':
-                        return <TextField id="outlined-basic" label={input} variant="outlined" value={state.phone} key={input} type='number' />
+                        return (
+
+                            <TextField id="outlined-basic" label={input} variant="outlined" value={state.phone} key={input} InputLabelProps={{ style: { fontSize: labelsize } }} inputProps={{ style: { fontSize: fontsize } }} type='number' />
+
+                        )
+                    case 'fname':
+                        return (
+
+                            <TextField id="outlined-basic" name={input} label={input} variant="outlined" onChange={onTextChange(input)} value={state.fname} key={input} InputLabelProps={{ style: { fontSize: labelsize } }} inputProps={{ style: { fontSize: fontsize } }} />
+
+                        )
+                    case 'lname':
+                        return (
+
+                            <TextField id="outlined-basic" label={input} variant="outlined" value={state.lname} key={input} InputLabelProps={{ style: { fontSize: labelsize } }} inputProps={{ style: { fontSize: fontsize } }} />
+
+                        )
+
+                    case 'address':
+                        return (
+
+                            <TextField id="outlined-basic" label={input} variant="outlined" value={state.address} key={input} InputLabelProps={{ style: { fontSize: labelsize } }} inputProps={{ style: { fontSize: fontsize } }} />
+
+                        )
+
                     default:
-                        return <TextField id="outlined-basic" label={input} variant="outlined" value={state.input} key={input} />
+                        return
                 }
             })
         )
-
     }
 
-    // useEffect(() => {
-    //     initialState()
-    // }, [])
+    useEffect(() => {
+        initialState()
+    }, [])
 
     return (
         <div className={classes.container}>
