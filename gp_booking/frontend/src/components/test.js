04068ad93412b115ec2e6bb8de30cc622441f71e
Matrix105 <7129666-matrix105@users.noreply.gitlab.com>
2021-04-15 02:05:54 +0100

    merge

---
+++ gp_booking/frontend/src/components/test.js
@@ -0,0 +1,175 @@
+import * as React from "react";
+import axios from "axios";
+import { useState, useEffect } from "react";
+import PropTypes, { func } from "prop-types";
+import { makeStyles } from "@material-ui/core/styles";
+import Box from "@material-ui/core/Box";
+import Collapse from "@material-ui/core/Collapse";
+import IconButton from "@material-ui/core/IconButton";
+import Table from "@material-ui/core/Table";
+import TableBody from "@material-ui/core/TableBody";
+import TableCell from "@material-ui/core/TableCell";
+import TableContainer from "@material-ui/core/TableContainer";
+import TableHead from "@material-ui/core/TableHead";
+import TableRow from "@material-ui/core/TableRow";
+import Typography from "@material-ui/core/Typography";
+import Paper from "@material-ui/core/Paper";
+import KeyboardArrowDownIcon from "@material-ui/icons/KeyboardArrowDown";
+import KeyboardArrowUpIcon from "@material-ui/icons/KeyboardArrowUp";
+
+
+const useRowStyles = makeStyles({
+  root: {
+    "& > *": {
+      borderBottom: "unset",
+    },
+  },
+});
+
+const fetchApi = () => {
+  const [prescriptions, setPrescriptions] = useState("");
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState(null);
+
+  useEffect(async () => {
+    setIsLoading(true);
+    setError(false);
+
+    try {
+      const result = await axios("http://localhost:1337/prescriptions");
+      setPrescriptions(result.data);
+    } catch (error) {
+      setError(true);
+    }
+
+    setIsLoading(false);
+  });
+  return { prescriptions, isLoading, error };
+}
+
+function createData(fname, lname, email, dob, notes) {
+  return {
+    fname,
+    lname,
+    email,
+    dob,
+    notes,
+    medication: [
+      { name: "lore", strength: "11091700", quantity: 3 },
+      { name: "ipsum2", strength: "Anonymous", quantity: 1 },
+    ],
+  };
+}
+
+function Row(props) {
+  const { row } = props;
+  const [open, setOpen] = React.useState(false);
+  const classes = useRowStyles();
+
+  return (
+    <React.Fragment>
+      <TableRow className={classes.root}>
+        <TableCell>
+          <IconButton
+            aria-label="expand row"
+            size="small"
+            onClick={() => setOpen(!open)}
+          >
+            {open ? <KeyboardArrowUpIcon /> : <KeyboardArrowDownIcon />}
+          </IconButton>
+        </TableCell>
+        <TableCell component="th" scope="row">
+          {row.fname}
+        </TableCell>
+        <TableCell align="right">{row.lname}</TableCell>
+        <TableCell align="right">{row.email}</TableCell>
+        <TableCell align="right">{row.notes}</TableCell>
+      </TableRow>
+      <TableRow>
+        <TableCell style={{ paddingBottom: 0, paddingTop: 0 }} colSpan={6}>
+          <Collapse in={open} timeout="auto" unmountOnExit>
+            <Box margin={1}>
+              <Typography variant="h6" gutterBottom component="div">
+                Medication
+              </Typography>
+              <Table size="small" aria-label="purchases">
+                <TableHead>
+                  <TableRow>
+                    <TableCell>Name</TableCell>
+                    <TableCell>Strenght</TableCell>
+                    <TableCell align="right">Amount</TableCell>
+                  </TableRow>
+                </TableHead>
+                <TableBody>
+                  {row.medication.map((med) => (
+                    <TableRow key={med.name}>
+                      <TableCell component="th" scope="row">
+                        {med.name}
+                      </TableCell>
+                      <TableCell>{med.strength}</TableCell>
+                      <TableCell align="right">{med.quantity}</TableCell>
+                    </TableRow>
+                  ))}
+                </TableBody>
+              </Table>
+            </Box>
+          </Collapse>
+        </TableCell>
+      </TableRow>
+    </React.Fragment>
+  );
+}
+
+Row.propTypes = {
+  row: PropTypes.shape({
+    calories: PropTypes.number.isRequired,
+    carbs: PropTypes.number.isRequired,
+    fat: PropTypes.number.isRequired,
+    history: PropTypes.arrayOf(
+      PropTypes.shape({
+        amount: PropTypes.number.isRequired,
+        customerId: PropTypes.string.isRequired,
+        date: PropTypes.string.isRequired,
+      })
+    ).isRequired,
+    name: PropTypes.string.isRequired,
+    price: PropTypes.number.isRequired,
+    protein: PropTypes.number.isRequired,
+  }).isRequired,
+};
+
+// const rows = [
+//   createData("Frozen yoghurt", 159, 6.0, 24, 4.0, 3.99),
+//   createData("Ice cream sandwich", 237, 9.0, 37, 4.3, 4.99),
+//   createData("Eclair", 262, 16.0, 24, 6.0, 3.79),
+//   createData("Cupcake", 305, 3.7, 67, 4.3, 2.5),
+//   createData("Gingerbread", 356, 16.0, 49, 3.9, 1.5),
+// ];
+
+
+const rows = [
+    
+];
+
+export default function CollapsibleTable() {
+  return (
+    <TableContainer component={Paper}>
+      <Table aria-label="collapsible table">
+        <TableHead>
+          <TableRow>
+            <TableCell />
+            <TableCell>First Name</TableCell>
+            <TableCell align="right">Last Name</TableCell>
+            <TableCell align="right">Email</TableCell>
+            <TableCell align="right">Notes</TableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {rows.map((row) => (
+            <Row key={row.name} row={row} />
+          ))}
+        </TableBody>
+      </Table>
+    </TableContainer>
+  );
+}
