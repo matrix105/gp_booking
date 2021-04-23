7ecc7b50b1c5445890a2a6649fc06e81d07131ba
Matrix105 <7129666-matrix105@users.noreply.gitlab.com>
2021-04-23 02:37:51 +0100

    fixes

--- gp_booking/frontend/src/components/Team.jsx
+++ gp_booking/frontend/src/components/Team.jsx
@@ -3,19 +3,6 @@ import axios from "axios";
 import { DataGrid } from "@material-ui/data-grid";
 import { Jumbotron } from "reactstrap";
 
-const columns = [
-  { field: "id", headerName: "ID", width: 70 },
-  { field: "fname", headerName: "First name", width: 160 },
-  { field: "lname", headerName: "Last name", width: 160 },
-  { field: "email", headerName: "email", width: 250 },
-  {
-    field: "dob",
-    headerName: "Date of Birth",
-    type: "string",
-    width: 160,
-  },
-  { field: "role", headerName: "Role", width: 250 },
-];
 
 export default function Team() {
   return (
@@ -31,13 +18,12 @@ export default function Team() {
           <div class="card">
             <img
               src="http://139.59.188.122:1337/uploads/phonepicutres_TA_56a1a705f2.jpg"
-              alt="John"
+              alt="Deepesh"
             />
             <div class="container">
               <h2>Deepesh Patel</h2>
               <p class="title">Project Manager</p>
-              <p>Some text that describes me lorem ipsum ipsum lorem.</p>
-              <p>18181818</p>
+              <p>1812328</p>
             </div>
           </div>
         </div>
@@ -51,8 +37,7 @@ export default function Team() {
             <div class="container">
               <h2>Kathan Sheth</h2>
               <p class="title">Team Manager</p>
-              <p>Some text that describes me lorem ipsum ipsum lorem.</p>
-              <p>mike@example.com</p>
+              <p>1818664</p>
             </div>
           </div>
         </div>
@@ -61,13 +46,12 @@ export default function Team() {
           <div class="card">
             <img
               src="http://139.59.188.122:1337/uploads/phonepicutres_TA_56a1a705f2.jpg"
-              alt="John"
+              alt="Jubeen"
             />
             <div class="container">
               <h2>Jubeen Amatya</h2>
               <p class="title">Risk Controller</p>
-              <p>Some text that describes me lorem ipsum ipsum lorem.</p>
-              <p>john@example.com</p>
+              <p>1818357</p>
             </div>
           </div>
         </div>
@@ -78,13 +62,12 @@ export default function Team() {
           <div class="card">
             <img
               src="http://139.59.188.122:1337/uploads/phonepicutres_TA_56a1a705f2.jpg"
-              alt="John"
+              alt="Umar"
             />
             <div class="container">
               <h2>Umar Sanusi</h2>
               <p class="title">Quality Assurer</p>
-              <p>Some text that describes me lorem ipsum ipsum lorem.</p>
-              <p>john@example.com</p>
+              <p>1811726</p>
             </div>
           </div>
         </div>
@@ -92,12 +75,11 @@ export default function Team() {
           <div class="card">
             <img
               src="http://139.59.188.122:1337/uploads/phonepicutres_TA_56a1a705f2.jpg"
-              alt="John"
+              alt="Marwan"
             />
             <div class="container">
               <h2>Marwan Tourky</h2>
               <p class="title">Development Manager</p>
-              <p>Some text that describes me lorem ipsum ipsum lorem.</p>
               <p>1816281</p>
             </div>
           </div>
